03bfa4d86087ce9c8362cae82b7888e0f363345c
qilu <qiluxt@163.com>
2022-11-01 14:41:35 +0800

    Initial commit

---
+++ collector.py
@@ -0,0 +1,43 @@
+from github import Github
+from datetime import datetime, timedelta
+
+
+class Model:
+
+    def __init__(self, token, repositoryName):
+        self.token = token
+        self.repositoryName = repositoryName
+
+    def get_offline_data(self):
+        historys = []
+        stargazers = self.get_starred_strangers()
+
+        for i, people in enumerate(stargazers):
+            historys.append(datetime.timestamp(people.starred_at))
+            if i % 5000 == 0:
+                with open("result" + str(i) + ".txt", "w") as f:
+                    f.writelines("\n".join('%s' % a for a in historys))
+                    historys = []
+
+    def get_online_data_per_hour(self):
+        historys = []
+        stargazers = self.get_starred_strangers()
+        starred_at_list = [datetime.timestamp(people.starred_at) for people in stargazers]
+        starred_at_list.sort(reverse=True)
+
+        timestamp_one_hour_ago = datetime.timestamp((datetime.now() - timedelta(hours=9)))
+
+        for ts in starred_at_list:
+            if ts >= timestamp_one_hour_ago:
+                historys.append(ts)
+            else:
+                break
+        with open("resultRecentOneHour.txt", "w") as f:
+            f.writelines("\n".join('%s' % a for a in historys))
+            historys = []
+
+
+    def get_starred_strangers(self):
+        g = Github(self.token)
+        repo = g.get_repo(self.repositoryName)  # 仓库名
+        return repo.get_stargazers_with_dates()
---
+++ main.py
@@ -0,0 +1,25 @@
+from collector import Model
+import time
+import schedule
+
+
+MODEL_TYPE = 'online'
+TOKEN = "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"
+REPOSITORY_NAME = 'sdp-sig/sdp-sig.github.io'
+
+if __name__ == '__main__':
+    collector = Model(TOKEN, REPOSITORY_NAME)
+    if MODEL_TYPE == 'offline':
+        collector.get_offline_data()
+    elif MODEL_TYPE == 'online':
+        schedule.every().minute.do(collector.get_online_data_per_hour)
+        while True:
+            schedule.run_pending()
+
+    # timestamp = 1667253727.0
+    #
+    # #转换成localtime
+    # time_local = time.localtime(timestamp)
+    # #转换成新的时间格式(2016-05-05 20:28:54)
+    # dt = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
+    # print(dt)
